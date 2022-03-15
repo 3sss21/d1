9430104ced3d55e6de9aa60ced48bb7c05220a55
3sss21 <asultanoov@gmail.com>
2022-03-15 13:45:58 +0300

    homework

---
+++ bin/homework.dart
@@ -0,0 +1,42 @@
+void main(List<String> arguments) {
+  // String
+ /* String name, surname, city, male, phoneNumber;
+  name = 'Aidar';
+  surname ="Sultanov";
+  city = "Toronto";
+  male = "man";
+  phoneNumber = "0312";
+  print(name);
+  print(surname);
+  print(city);
+  print(phoneNumber);
+  print(male);*/
+
+  // integer
+
+  /*int num1 = 23;
+  int num2 =32;
+  double num3 = 123;
+  double num4  = 2.4;
+  print(num1);
+  print(num2);
+  print(num3);
+  print(num4);*/
+
+  // list
+//   List array1 = [1, 2, true, {'age':23}, 1, 1.234 ];
+// print(array1[0]);
+// print(array1[2]);
+// print(array1[3].runtimeType);
+/*final i =0;
+var sum = 1;
+sum = 23;
+const number = 23;
+// number =24;
+dynamic smth = [1,1,2,3];
+print(i);
+print(sum);
+print(number);
+print(smth);
+*/
+}
